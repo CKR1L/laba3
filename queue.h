f94ddc009cd0d9e55605162d8271ae16c33622c5
CKR1L <kirill.zapruta@yandex.ru>
2025-12-14 14:36:16 +0300

    Add files via upload

---
+++ queue.h
@@ -0,0 +1,17 @@
+typedef struct Elem {
+    int inf;
+    struct Elem* link;
+}Elem;
+
+typedef struct Queue{
+    Elem* BegQ;
+    Elem* EndQ;
+    int size;
+} Queue;
+Queue* create_queue();
+void add_elem_queue(Queue* p, int value);
+int remove_elem_queue(Queue *p);
+int is_empty(Queue *p);
+void free_queue(Queue *p);
+void print_queue(Queue *p);
+Queue* array_to_queue(int arr[], int size);
---
+++ sorting.h
@@ -0,0 +1,4 @@
+#include "queue.h"
+void selection_sort(int arr[], int n);
+void quick_sort(int arr[], int low, int high);
+int queue_to_array(Queue* p, int* size);
