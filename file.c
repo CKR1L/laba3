ca0f418aa6b0d38f1d68f5ad4d945e1f1bd2a3b1
CKR1L <kirill.zapruta@yandex.ru>
2025-12-21 18:32:23 +0300

    Update file.c

--- file.c
+++ file.c
@@ -3,56 +3,39 @@
 #include <time.h>
 #include "file.h"
 
-void write_array_to_file(int arr[], int size, const char* data){
-    FILE*file = fopen(data, "w");
-    if (file == NULL){
-        printf("Ошибка при открытии файла", data);
-        return 0;
+void write_array_to_file(int arr[], int size, const char* filename) {
+    FILE* file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("Ошибка при открытии файла: %s\n", filename);
+        return;
     }
-    for(int i = 0; i < size; i++){
-        fpirnf(file, "%d", arr[i]);
-        if (i < size - 1){
-            fprinf(file, " ");
+    for (int i = 0; i < size; i++) {
+        fprintf(file, "%d", arr[i]);
+        if (i < size - 1) {
+            fprintf(file, " ");
         }
     }
     fclose(file);
 }
-int read_array_from_file(const char* data, int size){
-    FILE* file = fopen(data, "r");
-    if (file == NULL){
-        printf("Ошибка открытия файла", data);
-        size = 0;
+
+int* read_array_from_file(const char* filename, int* size) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Ошибка открытия файла: %s\n", filename);
+        *size = 0;
         return NULL;
     }
     int capacity = 100;
-    int* arr = (int)malloc(capacity * sizeof(int));
-    size = 0;
+    int* arr = (int*)malloc(capacity * sizeof(int));
+    *size = 0;
     int num;
-    while (fscanf(file, "%d", &num) == 1){
-        if (size >= capacity){
+    while (fscanf(file, "%d", &num) == 1) {
+        if (*size >= capacity) {
             capacity *= 2;
-            arr = (int)realloc(arr, capacity * sizeof(int));
+            arr = (int*)realloc(arr, capacity * sizeof(int));
         }
-        arr[(size)++] = num;
+        arr[(*size)++] = num;
     }
     fclose(file);
     return arr;
-}
-void generate_test_files(){
-    srand(time(NULL));
-    int sizes[] = {100, 500, 1000, 5000};
-    int num_sizes = sizeof(sizes) / sizeof(sizes[0]);
-    for (int i = 0; i <num_sizes; i++){
-        char data[50];
-        sprintf(data, "test_data.txt", sizes[i]);
-        FILE* file = fopen(data, "w");
-        for (int j = 0; j < sizes[i]; j++){
-            fprintf(file, "%d", rand()%1000);
-            if (j < sizes[i] - 1){
-                fprintf(file, " ");
-            }
-        }
-        fclose(file);
-        printf("Сгенерирован тестовый файл", data);
-    }
 }
