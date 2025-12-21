6bed6cf895efe8e1eaaa67fd775038d26e69da70
CKR1L <kirill.zapruta@yandex.ru>
2025-12-21 18:28:56 +0300

    Update sorting.c

--- sorting.c
+++ sorting.c
@@ -2,52 +2,57 @@
 #include <stdlib.h>
 #include "sorting.h"
 #include "queue.h"
-void selection_sort(int arr[], int n){
-    for(int i = 0; i < n - 1; i++){
+
+void selection_sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
         int min_idx = i;
-        for(int  j = i + 1; j < n; j++){
-            if(arr[j] < arr[min_idx]){
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
                 min_idx = j;
             }
         }
-        int inf = arr[min_idx];
+        int tmp = arr[min_idx];
         arr[min_idx] = arr[i];
-        arr[i] = inf;        
+        arr[i] = tmp;        
     }
 }
-int partition(int arr[], int low, int high){
+
+int partition(int arr[], int low, int high) {
     int pivot = arr[high];
     int i = (low - 1);
-    for (int j = low; j <= high - 1; j++){
-        if (arr[j] < pivot){
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
             i++;
-            int inf = arr[i];
+            int tmp = arr[i];
             arr[i] = arr[j];
-            arr[j] = inf;
+            arr[j] = tmp;
         }
     }
-    int inf = arr[i+1];
-        arr[i+1] = arr[high];
-        arr[high] = inf;
-        return (i+1);
+    int tmp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = tmp;
+    return (i + 1);
 }
-void quick_sort(int arr[], int low, int high){
-    if(low < high){
+
+void quick_sort(int arr[], int low, int high) {
+    if (low < high) {
         int pi = partition(arr, low, high);
         quick_sort(arr, low, pi - 1);
         quick_sort(arr, pi + 1, high);
     }
 }
-int queue_to_array(Queue* p, int* size){
+
+int* queue_to_array(Queue* p, int* size) {
     *size = p->size;
-    int arr = (int*)malloc(*size*sizeof(int));
-    if (arr == NULL){
+    int* arr = (int*)malloc(*size * sizeof(int));
+    if (arr == NULL) {
         printf("Ошибка при выделении памяти!");
+        return NULL;
     }
     Elem* current = p->BegQ;
-    for (int i = 0; i < size; i++){
+    for (int i = 0; i < *size; i++) {
         arr[i] = current->inf;
         current = current->link;
     }
     return arr;
-}   
+}
