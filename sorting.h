d2132ad9d838f9deda5ac5b6d02c9f3af0b19f03
CKR1L <kirill.zapruta@yandex.ru>
2025-12-21 18:30:46 +0300

    Update sorting.h

--- sorting.h
+++ sorting.h
@@ -1,4 +1,7 @@
+#ifndef SORTING_H
+#define SORTING_H
 #include "queue.h"
 void selection_sort(int arr[], int n);
 void quick_sort(int arr[], int low, int high);
-int queue_to_array(Queue* p, int* size);
+int* queue_to_array(Queue* p, int* size);
+#endif
